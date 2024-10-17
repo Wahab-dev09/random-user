dd940133301f0033d4f22085cec8be0b02279357
Wahab-dev09 <wahabzsec@gmail.com>
2024-10-17 03:25:11 -0600

    Added Comments

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const fetchRandomUser = async () => {
-  const button = document.getElementById('generateButton');
+const fetchRandomUser = async () => {  // this fucntion will run on click only 
   const buttonText = document.getElementById('buttonText');
   const loaderIcon = document.getElementById('loaderIcon');
   loaderIcon.style.display = 'inline-block';
-  buttonText.style.display = 'none';
+  buttonText.style.display = 'none'; // hides text after click and show loader
 
   try {
     const reponse = await fetch('https://randomuser.me/api/');
-    const object = await reponse.json();
-    const { results } = object;
-    const user = results[0];
+    const object = await reponse.json(); 
+    const { results } = object;     // Destructured the object here
+    const user = results[0];        // Stored user details in user variable
     document.getElementById('name').textContent = `${user.name.first} ${user.name.last}`;
     document.getElementById('gender').textContent = user.gender;
     document.getElementById('age').textContent = user.dob.age;
@@ -18,8 +17,9 @@ const fetchRandomUser = async () => {
   } catch (error) {
     console.error('Error fetching user data:', error);
   } finally {
-   // Hide loader icon and show button text back to default
    loaderIcon.style.display = 'none';
-   buttonText.style.display = 'inline'; // Show button text again
+   buttonText.style.display = 'inline'; // will show text after getting data
   }
 };
+
+// You can console log every variable in order to see whats going on, in each step!
